2f744182dfffdd76ae987fb12baa7a745472c2aa
Mats Törnberg <mats.tornberg@tretton37.com>
2021-06-07 13:51:11 +0200

    routes for toplist

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import Words from "./Components/Words";
 import "./App.css";
 import { texts } from "./texts";
 import useWord from "./useWord";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const TypingTest = ({ input, shouldShuffle }: TestProps) => {
   const { current, inputChange, wpm, done, words, wordIdx } = useWord(
@@ -41,18 +41,25 @@ function App() {
       <Router>
         <div id="menu">
           {texts.map(({ title, path }) => (
-            <Link key={title} to={path} className="button">
+            <Link key={`menuitem-${title}`} to={path} className="button">
               {title}
             </Link>
           ))}
         </div>
         {texts.map(({ path, title, ...selectedTest }) => (
-          <Route path={path}>
-            <TypingTest {...selectedTest} />
-          </Route>
+          <>
+            <Route key={`r-${title}`} path={path} exact>
+              <h2>{title}</h2>
+              <TypingTest {...selectedTest} />
+            </Route>
+            <Route key={`rtop-${title}`} path={path + "/top"} exact>
+              <h2>{title}</h2>
+              <p>Slaska</p>
+            </Route>
+          </>
         ))}
         <a href="https://github.com/matst80/code-typing-test">
-          <img width="80" src="github.png" alt="Github" />
+          <img width="80" src="/github.png" alt="Github" />
         </a>
       </Router>
     </div>
